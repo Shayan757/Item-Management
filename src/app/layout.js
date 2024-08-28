dcd7947a961ae132e914b5f2833668dafe497470
Shayan Ashraf <shayanashraf295@gmail.com>
2024-08-28 20:09:09 +0500

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+// layout.js
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ItemState from "../Context/itemState";  // Import your context
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ItemState>
+          {children}
+        </ItemState>
+      </body>
     </html>
   );
 }
